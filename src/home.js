11420a0d9db09dae4e18c8ee02db70b599484f57
CrispysCode <pr0ject0dinx@gmail.com>
2025-02-15 18:43:53 -0600

    Completed homepage using DOM manipulation in home.js

--- src/home.js
+++ src/home.js
@@ -11,11 +11,26 @@ export function homeModule() {
     header.appendChild(headerText);
     headerText.textContent = "Tokyo Hibachi";
 
-    
+
     const body = document.createElement("div");
+    body.classList.add('body-container');
     const logo = document.createElement("img");
     logo.classList.add('logo');
     logo.src = logoImg;
     content.appendChild(body);
     body.appendChild(logo);
+
+    const homeMsg = document.createElement("p");
+    homeMsg.textContent = "We are serving handmade burger and Japanese cuisine such as Hibachi, Lo mein noodle, fried rice and Sushi. We offer carry out service and catering only.";
+
+    body.appendChild(homeMsg);
+
+    const footer = document.createElement("footer");
+    content.appendChild(footer);
+
+    const footerMsg = document.createElement("p");
+    footerMsg.textContent = "Copyright Tokyo Hibachi 2025";
+    footerMsg.classList.add('footerMsg');
+    footer.appendChild(footerMsg);
+
 }
